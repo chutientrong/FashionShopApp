f487bd3a18a482bb4ac5e7764af34e9c6f1b4135
chutientrong <trongct.uit@gmail.com>
2022-09-04 14:34:51 +0700

    done Explore

---
+++ src/models/Explore.js
@@ -0,0 +1,71 @@
+export const manexplore = [
+  {
+    id: "0",
+    title: "MAN Shirt",
+    image: require("../assets/icons/category/man-shirt.png"),
+  },
+  {
+    id: "1",
+    title: "Man Work Equipment",
+    image: require("../assets/icons/category/man-work-equipment.png"),
+  },
+  {
+    id: "2",
+    title: "Man T-Shirt",
+    image: require("../assets/icons/category/man-tshirt.png"),
+  },
+  {
+    id: "3",
+    title: "Man Shoes",
+    image: require("../assets/icons/category/man-shoes.png"),
+  },
+  {
+    id: "4",
+    title: "Man Pants",
+    image: require("../assets/icons/category/man-pants.png"),
+  },
+  {
+    id: "5",
+    title: "Man Underwear",
+    image: require("../assets/icons/category/man-underwear.png"),
+  },
+];
+export const womanexplore = [
+  {
+    id: "0",
+    title: "Dress",
+    image: require("../assets/icons/category/dress.png"),
+  },
+  {
+    id: "1",
+    title: "Woman T-Shirt",
+    image: require("../assets/icons/category/woman-tshirt.png"),
+  },
+
+  {
+    id: "2",
+    title: "Woman Pants",
+    image: require("../assets/icons/category/woman-pants.png"),
+  },
+  {
+    id: "3",
+    title: "Skirt",
+    image: require("../assets/icons/category/skirt.png"),
+  },
+  {
+    id: "4",
+    title: "Woman Bag",
+    image: require("../assets/icons/category/woman-bag.png"),
+  },
+
+  {
+    id: "5",
+    title: "High Heels",
+    image: require("../assets/icons/category/high-heels.png"),
+  },
+  {
+    id: "6",
+    title: "Bikini",
+    image: require("../assets/icons/category/bikini.png"),
+  },
+];
--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { COLORS, FONTS } from '../constants'
-import Header from '../components/Header'
-import SearchInput from '../components/SearchInput'
+import { View, Text, FlatList, ScrollView } from "react-native";
+import React from "react";
+import { COLORS, FONTS, WIDTH } from "../constants";
+import Header from "../components/Header";
+import SearchInput from "../components/SearchInput";
 import Icon from "react-native-vector-icons/Ionicons";
+import CategoryCard from "../components/CategoryComponents/CategoryCard";
+import { manexplore, womanexplore } from "../models/Explore";
+import ExploreCard from "../components/ExploreCard";
 
 const Explore = () => {
+  // console.log("fulldata " , fulldata(manexplore))
+  // add null
+  // const a={
+  //   id: "",
+  //   title: "",
+  //   image: null,
+  // }
+  // const fulldata=(data)=>{
+  //   if(data.length %4 !=0){
+  // data.push({a})
+  // fulldata(data)
+  //   }else{
+  //     return data
+  //   }
+  // }
+  // console.log("fulldata " , fulldata(manexplore))
+
+  // const modifyData = (data) => {
+  //   const numColumns = 4;
+  //   var tmp = {};
+  //   console.log("data.length ", data.length);
+  //   for (let i = data.length; ; i++) {
+  //     if (i % numColumns != 0) {
+  //       data.push(tmp);
+  //     }
+  //     return data;
+  //   }
+  // };
+  // console.log("fulldata ", modifyData(womanexplore));
+
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.white }}>
       <Header
@@ -44,6 +77,7 @@ const Explore = () => {
           </View>
         }
       ></Header>
+
       {/* ADD LINE */}
       <View
         style={{
@@ -51,21 +85,81 @@ const Explore = () => {
           backgroundColor: COLORS.neutralLight,
         }}
       />
-<View>
-  <View>
-    <Text style={{
-                    color: COLORS.neutralDark,
-                    fontSize: 14,
-                    fontWeight: "700",
-                    ...FONTS.fontPopinRegular,
-                    lineHeight: 18,
-                    letterSpacing: 0.5,
-                  }}> </Text>
-  </View>
-</View>
-      
-    </View>
-  )
-}
+      <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 } }>
+        {/* 1ND */}
+        <View style={{ flex: 1, height: 265, margin: 16 }}>
+          <Text
+            style={{
+              color: COLORS.neutralDark,
+              fontSize: 14,
+              fontWeight: "700",
+              ...FONTS.fontPopinRegular,
+              lineHeight: 18,
+              letterSpacing: 0.5,
+            }}
+          >
+            Man Fashion
+          </Text>
+          {/* <Text>{manexplore}</Text> */}
 
-export default Explore
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ width: "100%" } }>
+            <FlatList
+              style={{ width: WIDTH }}
+              numColumns={4}
+              data={womanexplore}
+              // columnWrapperStyle={{width:"100%",margin:16}}
+              renderItem={({ item }) => (
+                <View style={{ marginRight: 16, marginTop: 8 }}>
+                  <ExploreCard
+                    image={item.image}
+                    title={item.title}
+                    onPress={() => console.log(item.title)}
+                  ></ExploreCard>
+                </View>
+              )}
+            />
+            </ScrollView>
+    
+        </View>
+        {/* 2ND */}
+        <View style={{ flex: 1, height: 265, paddingHorizontal: 16 }}>
+          <Text
+            style={{
+              color: COLORS.neutralDark,
+              fontSize: 14,
+              fontWeight: "700",
+              ...FONTS.fontPopinRegular,
+              lineHeight: 18,
+              letterSpacing: 0.5,
+            }}
+          >
+            Woman Fashion
+          </Text>
+          {/* <Text>{manexplore}</Text> */}
+
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ width: "100%" }}>
+            <FlatList
+              style={{ width: WIDTH }}
+              numColumns={4}
+              data={womanexplore}
+              // columnWrapperStyle={{width:"100%",margin:16}}
+              renderItem={({ item }) => (
+                <View style={{ marginRight: 16, marginTop: 8 }}>
+                  <ExploreCard
+                    image={item.image}
+                    title={item.title}
+                    onPress={() => console.log(item.title)}
+                  ></ExploreCard>
+                </View>
+              )}
+            />
+          </ScrollView>
+        </View>
+      </ScrollView>
+
+      <View style={{ marginBottom: 60 }}></View>
+    </View>
+  );
+};
+
+export default Explore;
